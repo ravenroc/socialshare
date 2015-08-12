6c033527045766eb8c466f97df38ee6288585928
digsite <paris.jeremyj@gmail.com>
2015-08-12 18:13:27 -0400

    conflict fixing

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            build: {
+                src: 'jquery.socialshare.js',
+                dest: 'jquery.socialshare.min.js'
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['jquery.socialshare.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                },
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['uglify', 'watch']);
+
+};
